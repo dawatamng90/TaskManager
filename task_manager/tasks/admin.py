b01c0a01cf595421de725c5ffc6812795a40108a
Dawa Tamang <96434573+dawatamng90@users.noreply.github.com>
2025-01-24 11:45:18 +0530

    Delete task_manager directory

--- task_manager/tasks/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Task
-# Register your models here.
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id','title','description','completed']
-
-admin.site.register(Task,TaskAdmin)
